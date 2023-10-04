b5ba62e32615a8c3a91adc586663e28835ae7f0f
Achyuth2459 <achyuthchamakuri2459@gmail.com>
2023-10-04 12:33:57 +0530

    final changes

--- outpatient_appointment_system/outpatient_appointment_system/wsgi.py
+++ outpatient_appointment_system/outpatient_appointment_system/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outpatient_appointment_system.settings')
 
 application = get_wsgi_application()
+app=application
