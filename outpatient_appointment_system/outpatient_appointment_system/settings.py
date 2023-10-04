6bf47592c71b25a56af4ad1af97fe0060b88251f
Achyuth2459 <achyuthchamakuri2459@gmail.com>
2023-10-04 12:59:33 +0530

    changes

--- outpatient_appointment_system/outpatient_appointment_system/settings.py
+++ outpatient_appointment_system/outpatient_appointment_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=k6ctz1^w2n#mdc@d3paq+j!fst*&#xm&%nv$ph2l5-_(#++=g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
